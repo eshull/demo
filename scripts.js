f663d39d6e045a03f9773779e7516a0c37609b89
Eric Shull <eshulldesign@gmail.com>
2019-01-08 12:05:58 -0800

    add hover style to links in header and search form expansion on hover

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("Script ran");
+$(document).ready(function() {
+    console.log("Script ran");
+
+    // Search hover
+    $("#search").hover(function(){
+        $('#form').animate({width: "70px"});
+        }, function(){
+        $('#form').animate({width: "0px"});
+    });
+});
