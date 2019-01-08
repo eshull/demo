241928b662f79ec625295ff57bbd274ce2906df5
Eric Shull <eshulldesign@gmail.com>
2019-01-08 13:14:44 -0800

    add sticky header on scroll

--- scripts.js
+++ scripts.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
         }, function(){
         $('#form').animate({width: "0px"});
     });
+
+    // nav change on scroll 
+    window.onscroll = function() {navChange()};
+
+    var header = document.getElementById("nav");
+    var sticky = header.offsetTop;
+
+    function navChange() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+    }
 });
